da9c927a70b3b139a0c143acfbfde12eb4bdf9b6
Daniel Haanpaa <dhaanpaa@djhent.us>
2026-05-16 19:44:28 -0700

    Added support for local script repo

--- scripts/runMongoShell.sh
+++ scripts/runMongoShell.sh
@@ -9,6 +9,15 @@ if [ -z "$MONGO_URI" ]; then
     exit 1
 fi
 
+## Setup Local Repo for scripts to make available
+if [ ! -z "$GIT_SCRIPT_REPO" ]; then
+    mkdir -pv /work/local-scripts
+    cd /work/local-scripts
+    apt update
+    apt install -y git-core
+    git clone "$GIT_SCRIPT_REPO" /work/local-scripts
+fi
+
 ## Check for external script
 if [ ! -z "$EXTERNAL_SCRIPT" ]; then    
     echo "Using external script '$EXTERNAL_SCRIPT'"
@@ -19,9 +28,15 @@ if [ ! -z "$EXTERNAL_SCRIPT" ]; then
         exit 1
     fi
     echo "Downloaded external script to '$SCRIPT_FILE'"
+else
+    SCRIPT_FILE="$1"
 fi
 
-## Che
 
-mongosh "$MONGO_URI" "$SCRIPT_FILE"
-exit 0
+if [ -z "$SCRIPT_FILE" ]; then
+    echo "Script file not found"
+    exit 1
+else 
+    mongosh "$MONGO_URI" "$SCRIPT_FILE"
+    exit 0
+fi
